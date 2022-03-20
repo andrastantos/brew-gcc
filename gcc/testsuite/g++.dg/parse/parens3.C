3906d5f3526eec43ba6be62a27f7d7c1a576ac28
Andras Tantos <andras@tantosonline.com>
2022-03-19 17:05:44 -0700

    Test updates: $fp and $sp are really reserved.

--- gcc/testsuite/g++.dg/parse/parens3.C
+++ gcc/testsuite/g++.dg/parse/parens3.C
@@ -5,8 +5,11 @@ struct s {
   int i;
 };
 
-#ifdef __hppa__ 
+#ifdef __hppa__
 /* Register %r1 can't be fixed when generating PIC code.  */
+register struct s *reg __asm__( "4" );
+#elif defined __BREW__
+/* Register $r1 is the frame pointer, which can't be used as a variable. */
 register struct s *reg __asm__( "4" );
 #else
 register struct s *reg __asm__( "1" );
